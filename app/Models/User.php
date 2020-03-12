84e3e6a296afdabb0204712642814e353ceab65f
yukihamu1206 <iida.yuki.infratop@gmail.com>
2020-03-12 17:43:59 +0900

    API index.blade.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','profile_image','screen_name'
+        'name', 'email', 'password','profile_image','screen_name','api_token'
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,6 @@ use Illuminate\Http\Request;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
 });
 
 Route::resource('tweets','TweetsController',['ouly' => ['index','create','store','show','edit','update','destroy']]);
