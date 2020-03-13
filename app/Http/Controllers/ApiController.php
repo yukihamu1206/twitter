167f9a620b72f3cec4a02f8684b12eb1cfb1b9b9
yukihamu1206 <iida.yuki.infratop@gmail.com>
2020-03-13 10:39:45 +0900

    tweet_get でshowページ tweet_post で新規投稿

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tweet;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+
+    public function tweets(){
+
+        $tweets = Tweet::all();
+
+        $lists = [];
+
+        foreach ($tweets as $tweet){
+            $elm = [
+                'id' => $tweet->id,
+                'text' => $tweet->text,
+                'comment' => $tweet->comments,
+                'favorite_count' => $tweet->favorites->count()
+            ];
+
+            $lists[] = $elm;
+        }
+
+        return response()->json(
+            ['data' => $lists],
+            200,[],
+            JSON_UNESCAPED_UNICODE);
+    }
+
+    public function tweet_post(){
+
+        $tweet = new Tweet;
+        $tweet->text = 'おほほほ';
+        $tweet->user_id = Auth()->user()->id;
+
+        $tweet->save();
+
+        return response()->json(
+            ['data' => $tweet],
+            200,[],
+            JSON_UNESCAPED_UNICODE);
+    }
+
+    public function tweet_get(Request $request, $id){
+
+         $tweet = Tweet::find($id);
+
+        return response()->json(
+            ['data' => $tweet],
+            200,[],
+            JSON_UNESCAPED_UNICODE);
+
+    }
+}
