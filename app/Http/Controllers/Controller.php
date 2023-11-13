8c42787590e5279b3772f96b285e9957647e31c4
yukihamu1206 <yukiiida1206@gmail.com>
2023-11-13 14:30:35 +0900

    aaaa

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,5 +9,6 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
+    // aaaaa
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
